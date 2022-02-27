61924ad6aeb72f52432e8c17204bf7d5680f2a7a
Evgeny Smagin <smagin@echo-company.ru>
2022-02-27 23:31:44 +0500

    add pages

---
+++ client/src/views/LoginPage/Component.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+});
+</script>
+
+<template>
+  <h1>Login page</h1>
+</template>
---
+++ client/src/views/RegistrationPage/Component.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RegistrationPage',
+});
+</script>
+
+<template>
+  <h1>Registration page</h1>
+</template>
