6cb940056485afca6c1b8e8b797948288697f716
Evgeny Smagin <smagin@echo-company.ru>
2022-02-27 23:34:39 +0500

    add router and brand-colors

---
+++ client/src/router/routes.ts
@@ -0,0 +1,34 @@
+import { RouteRecordRaw } from 'vue-router';
+
+import RouteNames from './routeNames';
+
+import MainLayout from '@/layouts/MainLayout';
+import HomePage from '@/views/HomePage/';
+import LoginPage from '@/views/LoginPage';
+import RegistrationPage from '@/views/RegistrationPage';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      {
+        name: RouteNames.HomePage,
+        path: '',
+        component: HomePage,
+      },
+      {
+        name: RouteNames.LoginPage,
+        path: '/login',
+        component: LoginPage,
+      },
+      {
+        name: RouteNames.RegistrationPage,
+        path: '/registration',
+        component: RegistrationPage,
+      },
+    ],
+  },
+];
+
+export default routes;
