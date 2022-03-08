fb9e44b22fe230072ca75e637081f8ea509eb52a
Evgeny Smagin <smagin@echo-company.ru>
2022-03-08 18:59:38 +0500

    refactor routes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import { MainLayout } from './layouts/MainLayout';
+
 export default defineComponent({
   name: 'App',
+  components: {
+    MainLayout,
+  },
 });
 </script>
 
 <template>
-  <router-view />
+  <MainLayout />
 </template>
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -2,32 +2,25 @@ import { RouteRecordRaw } from 'vue-router';
 
 import RouteNames from './routeNames';
 
-import { MainLayout } from '@/layouts/MainLayout';
 import { HomePage } from '@/views/HomePage/';
 import { LoginPage } from '@/views/LoginPage';
 import { RegistrationPage } from '@/views/RegistrationPage';
 
 const routes: RouteRecordRaw[] = [
   {
+    name: RouteNames.HomePage,
     path: '/',
-    component: MainLayout,
-    children: [
-      {
-        name: RouteNames.HomePage,
-        path: '',
-        component: HomePage,
-      },
-      {
-        name: RouteNames.LoginPage,
-        path: '/login',
-        component: LoginPage,
-      },
-      {
-        name: RouteNames.RegistrationPage,
-        path: '/registration',
-        component: RegistrationPage,
-      },
-    ],
+    component: HomePage,
+  },
+  {
+    name: RouteNames.LoginPage,
+    path: '/login',
+    component: LoginPage,
+  },
+  {
+    name: RouteNames.RegistrationPage,
+    path: '/registration',
+    component: RegistrationPage,
   },
 ];
 
